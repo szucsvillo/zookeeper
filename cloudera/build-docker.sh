653347a25b610d79c7882106e7e75c6d8f411800
Bukros Szabolcs <szabolcs@cloudera.com>
2022-05-23 10:37:33 +0200

    DWX-12208: Remove high CVE from Zookeeper
    change base image to reduce CVE probability
    
    Change-Id: I1f55cbead6431c2b007bb34dd66a2d060870ea36
    (cherry picked from commit 46440557d06570049692ea4b558ae5c3539c302e)

--- cloudera/build-docker.sh
+++ cloudera/build-docker.sh
@@ -8,6 +8,11 @@ mkdir docker-tmp
 cp cloudera/* docker-tmp
 cp zookeeper-assembly/target/apache-zookeeper-*-bin.tar.gz docker-tmp
 
+json=`curl -s "https://release.infra.cloudera.com/hwre-api/imageinfo?project=BASE_IMAGE&image_name=ubi8/openjdk-11-runtime"`
+tag=`echo $json | jq '[.[] | {tag: .image_tag, meta: .meta_id}] | sort_by(.meta) | last | .tag'`
+tag=`sed -e 's/^"//' -e 's/"$//' <<< $tag`
+sed -i "s/image_tag/$tag/g" docker-tmp/Dockerfile
+
 if [ -z "$REGISTRY" ]; then
   REGISTRY=${REGISTRY:-"docker-private.infra.cloudera.com/cloudera"}
 fi
